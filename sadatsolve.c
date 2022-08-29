db586f661033590b1016bae0342bfdf9d3b08d96
SDXCODER8 <66432898+SDXCODER8@users.noreply.github.com>
2022-08-29 09:08:34 +0600

    Create sadatsolve.c

---
+++ sadatsolve.c
@@ -0,0 +1,110 @@
+// probleam number 02
+
+#include <stdio.h>
+int main()
+{
+      int A,B,X ;
+      scanf("%d%d",&A,&B);
+      A,&B
+      
+      X = A+B;
+      printf("X = %d\n",X);
+      return 0;
+      
+}
+// probleam number 03
+
+#include <stdio.h>
+
+int main()
+{
+      int A = 10, B = 9, X;
+
+      X = A + B;
+
+      printf("X = %d\n", X);
+      A = -10, B = 4;
+      X = A + B;
+
+      printf("X = %d\n", X);
+      A = 15, B = -7;
+      X = A + B;
+
+      printf("X = %d", X);
+
+      return 0;
+      
+}
+
+// probleam number 04
+
+#include <stdio.h>
+
+int main()
+{
+      int A , B ,SOMA;
+      scanf(" %d %d",&A,&B);
+      
+      SOMA = A + B;
+      printf("SOMA = %d\n",SOMA);
+      return 0;
+      
+}
+
+// probleam number 05
+
+#include <stdio.h>
+int main()
+{
+      float A,n = 3.1416,R = 2.00 ;
+      
+      A = n*(R*R);
+      printf("A = %f\n",A);
+      return 0;
+      
+}
+// probleam number 06
+#include <stdio.h>
+int main()
+{
+   printf("---------------------------------------\n");
+   printf("|                                     |\n");
+   printf("|                                     |\n");
+   printf("|                                     |\n");
+   printf("|                                     |\n");
+   printf("|                                     |\n");
+   printf("---------------------------------------");
+   
+return 0;
+}
+
+
+// probleam number 07
+// #include <stdio.h>
+int main()
+{
+   printf("---------------------------------------\n");
+   printf("|        Roberto                      |\n");
+   printf("|                                     |\n");
+   printf("|        5786                         |\n");
+   printf("|                                     |\n");
+   printf("|        UNIFEI                       |\n");
+   printf("---------------------------------------\n");
+   
+return 0;
+}
+// probleam 08
+#include <stdio.h>
+int main()
+{
+   printf("---------------------------------------\n");
+   printf("|x = 35                               |\n");
+   printf("|                                     |\n");
+   printf("|               x = 35                |\n");
+   printf("|               x = 35                |\n");
+   printf("|                                     |\n");
+   printf("|                               x = 35|\n");
+   printf("---------------------------------------\n");
+   
+return 0;
+}
