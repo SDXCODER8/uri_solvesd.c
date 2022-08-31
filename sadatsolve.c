e496a3c0730418fb80594f7c332f5882c5a6f8a3
SDXCODER8 <66432898+SDXCODER8@users.noreply.github.com>
2022-08-31 12:56:04 +0600

    Update sadatsolve.c

--- sadatsolve.c
+++ sadatsolve.c
@@ -148,3 +148,15 @@ int main()
 
      return 0;
 }
+// Probleam 2755
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Ro'b'er\tto\\/\"\n");
+    printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+    printf("(^_-) (-_-) (-_^)\n");
+    printf("(\"_\") (\'.\')\n");
+   return 0;
+
+}
